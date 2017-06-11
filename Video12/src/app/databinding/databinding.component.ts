ffc0eaa0efa141c84a68231eddb871f408eb1695
nasouza2 <natalia.valladao@gmail.com>
2017-06-11 15:04:41 -0300

    Link do vídeo 12: https://www.youtube.com/watch?v=7PI-2pvLTu8&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G&index=13

---
+++ Video12/src/app/databinding/databinding.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding',
+  templateUrl: './databinding.component.html',
+  //styleUrls: ['./databinding.component.css']
+  styles: [
+    `
+      .highligth{
+       background-color: yellow;
+       font-weight: bold;
+                }
+    
+    `
+          ]
+})
+export class DatabindingComponent implements OnInit {  
+  url: string = 'http://loiane.com'; 
+  cursoAngular: boolean = true; 
+  urlImagem = 'http://lorempixel.com/400/200/nature/'; 
+
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
+  nome: string = 'abc';
+  pessoa: any ={
+    nome: 'def',
+    idade: 20
+  }
+
+  getValor(){
+    return 1;
+  }  
+  getCurtirCurso(){
+    return true;
+
+  }
+  BotaoClicado(){
+    alert('Botão clicado!');
+  }
+  
+  OnKeyUp(evento: KeyboardEvent){ 
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
