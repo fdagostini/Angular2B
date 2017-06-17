ea89063f418d01540d350aa0e0a637ae59392284
nasouza2 <natalia.valladao@gmail.com>
2017-06-17 00:27:01 -0300

    Link do vídeo 30: https://www.youtube.com/watch?v=WBf5sgByOY4&index=31&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G

---
+++ Video30/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgClassComponent } from './diretiva-ng-class/diretiva-ng-class.component';
+import { DiretivaNgStyleComponent } from './diretiva-ng-style/diretiva-ng-style.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgforComponent,
+    DiretivaNgClassComponent,
+    DiretivaNgStyleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
