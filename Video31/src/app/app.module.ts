c8d0ed4764083d920c15f99dbb8bb565151fbfee
nasouza2 <natalia.valladao@gmail.com>
2017-06-17 10:16:09 -0300

    Link do vídeo 31: https://www.youtube.com/watch?v=z2GUOnkGCdc&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G&index=32

---
+++ Video31/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgClassComponent } from './diretiva-ng-class/diretiva-ng-class.component';
+import { DiretivaNgStyleComponent } from './diretiva-ng-style/diretiva-ng-style.component';
+import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgforComponent,
+    DiretivaNgClassComponent,
+    DiretivaNgStyleComponent,
+    OperadorElvisComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
