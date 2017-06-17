db9bf44c4864fb55b9c46f449a87d48cada43c13
nasouza2 <natalia.valladao@gmail.com>
2017-06-16 23:29:41 -0300

    Link do vídeo 28: https://www.youtube.com/watch?v=e-OGZocglTA&index=29&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G

---
+++ Video28/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgforComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
