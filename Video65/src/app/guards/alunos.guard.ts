41235e2e019633b04fcf89c0926afcbd5f57dd7c
nasouza2 <natalia.valladao@gmail.com>
2017-06-25 19:50:21 -0300

    Link do vídeo 65: https://www.youtube.com/watch?v=e7ttQtj2w6E&index=66&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G

---
+++ Video65/src/app/guards/alunos.guard.ts
@@ -0,0 +1,26 @@
+
+import { CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router/src";
+import { Promise } from "@types/q";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+
+@Injectable()
+export class AlunosGuard implements CanActivateChild {
+
+    canActivateChild(
+        route: ActivatedRouteSnapshot,    
+        state: RouterStateSnapshot):
+        Observable<boolean>|boolean{
+            console.log(route);        
+            console.log(state);    
+
+            if(state.url.includes('editar')){
+                alert('Usuário sem acesso');
+                return false;
+            }
+            return true;
+        }
+}  
+
+
---
+++ Video65/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Usuario } from './usuario';
+import { AuthService } from './auth.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  private usuario: Usuario = new Usuario();
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+fazerLogin(){
+  //console.log(this.usuario);
+  this.authService.fazerLogin(this.usuario);
+
+}
+}
