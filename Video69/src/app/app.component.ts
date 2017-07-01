3dab9b1f203804e5046e17b93340749e990e0410
nasouza2 <natalia.valladao@gmail.com>
2017-06-30 21:13:36 -0300

    Link do vídeo 69: https://www.youtube.com/watch?v=SQViHeue6bs&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G&index=70

---
+++ Video69/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './login/auth.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  mostrarMenu: boolean = false;
+  constructor(private authService: AuthService){
+  }
+
+  ngOnInit(){
+    this.authService.mostrarmenuEmitter.subscribe(
+      mostrar => this.mostrarMenu = mostrar);
+  }
+}
