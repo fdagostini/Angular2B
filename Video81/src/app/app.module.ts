8534fcf7ef35175e5e9ae33a99742de84bed4c4a
nasouza2 <natalia.valladao@gmail.com>
2017-07-01 14:06:41 -0300

    Link do vídeo 81: https://www.youtube.com/watch?v=wO2YgE3mQXg&index=82&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G

---
+++ Video81/src/app/app.module.ts
@@ -0,0 +1,27 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
+import { TemplateFormComponent } from './template-form/template-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,   
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    TemplateFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
