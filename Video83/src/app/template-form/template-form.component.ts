46627379e8ba14713f58d895529797938bc6d451
nasouza2 <natalia.valladao@gmail.com>
2017-07-01 21:25:40 -0300

    Link do vídeo 83: https://www.youtube.com/watch?v=FXrOR84Equ4&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G&index=84

---
+++ Video83/src/app/template-form/template-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any ={
+    nome: null,
+    email: null
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+onSubmit(form){
+  console.log(form);  
+}
+
+verificaValidTouched(campo){
+  return !campo.valid && campo.touched;
+}
+
+aplicaCssErro(campo){
+  return{
+    'has-error': this.verificaValidTouched(campo),
+    'has-feedback': this.verificaValidTouched(campo)
+  }
+}
+
+}
